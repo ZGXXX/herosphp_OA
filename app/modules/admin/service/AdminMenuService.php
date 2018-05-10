2e8126fe775a0929e6df75a388547f32ddeeef4b
ZGXXX <605313192@qq.com>
2018-05-10 17:08:07 +0800

    权限初始化搭建完成

---
+++ app/modules/admin/service/AdminMenuService.php
@@ -0,0 +1,88 @@
+<?php
+namespace app\admin\service;
+
+use herosphp\cache\CacheFactory;
+use herosphp\cache\FileCache;
+use herosphp\model\CommonService;
+use app\admin\dao\AdminMenuDao;
+use herosphp\utils\ArrayUtils;
+
+/**
+ * 管理员菜单服务
+ * @author yangjian<yangjian102621@gmail.com>
+ */
+class AdminMenuService extends CommonService {
+
+	protected $modelClassName = AdminMenuDao::class;
+
+    const MENU_CACHE_KEY = "admin_menu_cache";
+
+    /**
+     * 判断登录人可以显示哪些菜单
+     */
+    public function getFirstMenu($items) {
+        $userPermissions =  array_keys($_SESSION[ LOGIN_USER_SESSION_KEY][permissions]);
+        foreach($items as $k => $v) {
+            foreach ($userPermissions as $k_in => $v_in) {
+                $permissions = array_column($v[subs],"permission");
+                if (!in_array($permissions,$userPermissions)) {
+                    unset($items[$k]);
+                }
+            }
+        }
+        return $items;
+    }
+
+    /**
+     * 菜单列表显示所有菜单（管理员可用）
+     */
+    public function showMenu() {
+        // 获取一级菜单
+        $topMenus = $this->order('sort ASC')->where('pid', 0)->find();
+        $menus = $this->order('sort ASC')->find();
+
+        foreach($topMenus as $k => $v) {
+            $topMenus[$k]['subs'] = ArrayUtils::filterArrayByKey('pid', $v['id'], $menus);
+        }
+        return $topMenus;
+    }
+
+    /**
+     * 获取菜单缓存
+     */
+    public function getMenuCache() {
+        $cacher = CacheFactory::create(FileCache::class);
+        $items = $cacher->get(self::MENU_CACHE_KEY);
+        if (empty($items)) {
+            $this->updateMenuCache();
+            return $cacher->get(self::MENU_CACHE_KEY);
+        }
+        return $items;
+    }
+
+    /**
+     * 更新菜单缓存
+     */
+    public function updateMenuCache() {
+        // 获取一级菜单
+        $topMenus = $this->order('sort ASC')->where('pid', 0)->find();
+        $menus = $this->order('sort ASC')->find();
+
+        $userMenus = array();
+        $userPermissions =  array_keys($_SESSION[ LOGIN_USER_SESSION_KEY][permissions]);
+        foreach ($userPermissions as $out_v){
+            foreach ($menus as $in_v){
+                if ($out_v == $in_v["permission"]) {
+                    $userMenus[] = $in_v;
+                }
+            }
+        }
+        foreach($topMenus as $k => $v) {
+            $topMenus[$k]['subs'] = ArrayUtils::filterArrayByKey('pid', $v['id'], $userMenus);
+        }
+
+        $cacher = CacheFactory::create(FileCache::class);
+        $cacher->set(self::MENU_CACHE_KEY, $topMenus, 0);
+    }
+
+}
---
+++ app/modules/admin/service/AdminService.php
@@ -0,0 +1,72 @@
+<?php
+namespace app\admin\service;
+use app\admin\dao\AdminDao;
+use herosphp\core\Loader;
+use herosphp\model\CommonService;
+use herosphp\session\Session;
+use herosphp\string\StringUtils;
+use herosphp\cache\CacheFactory;
+use herosphp\cache\FileCache;
+
+/**
+ * 管理员服务
+ * ----------------
+ * @author yangjian<yangjian102621@gmail.com>
+ */
+class AdminService extends CommonService {
+
+    protected $modelClassName = AdminDao::class;
+
+    // 用户登录 session key
+    const LOGIN_USER_SESSION_KEY = 'LOGIN_USER_SESSION_KEY';
+
+    const MENU_CACHE_KEY = "admin_menu_cache";
+    /**
+     * 用户登录服务
+     * @param $username
+     * @param $password
+     * @return array
+     */
+    public function login($username, $password) {
+        $item = $this->where('username', $username)->findOne();
+        $_password = genPassword($password, $item['salt']);
+        if ($_password != $item['password']) {
+            return false;
+        }
+
+        //获取用户角色和权限
+        $roleService = Loader::service(AdminRoleService::class);
+        $roleIds = StringUtils::jsonDecode($item['role_ids']);
+        $roles = $roleService->where('id', 'IN', $roleIds)->find();
+
+        $permissions = [];
+        foreach($roles as $value) {
+            $permissions = array_merge($permissions, StringUtils::jsonDecode($value['permissions']));
+        }
+        $item['permissions'] = $permissions;
+
+        Session::start();
+        Session::set(self::LOGIN_USER_SESSION_KEY, $item);
+        return $item;
+    }
+
+    /**
+     * 获取当前登录的管理员
+     */
+    public function getLoginManager() {
+
+        Session::start();
+        return Session::get(self::LOGIN_USER_SESSION_KEY);
+    }
+
+    /**
+     * 登出
+     */
+    public function logout() {
+        Session::start();
+        Session::set(self::LOGIN_USER_SESSION_KEY, null);
+        $cacher = CacheFactory::create(FileCache::class);
+        $cacher->delete(self::MENU_CACHE_KEY);
+    }
+
+}
